08bad73231ef02a9335c22e79da0107a1c5f5354
leo117 <leonard_xiao@hotmail.com>
2022-06-06 12:44:02 +0200

    Current process http://localhost:5001/<number> button actions are still missing

--- app.py
+++ app.py
@@ -7,9 +7,10 @@ app.config['SECRET_KEY'] = '0123456789'
 
 
 @app.route('/', methods=['GET', 'POST'], defaults={'port_num': 0})
-@app.route('/<port_num>', methods=['GET', 'POST'])
+@app.route('/<int:port_num>', methods=['GET', 'POST'])
 def index(port_num):
-    return render_template('index.html', port_num=port_num)
+    row_span = int(port_num) + 1
+    return render_template('index.html', port_num=port_num, row_span=row_span)
 
 
 if __name__ == '__main__':
